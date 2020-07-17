c93f469cea4aabf2055155d168d2cdd4470747c6
Eddieseet <13eddie07@gmail.com>
2020-07-17 23:04:28 +0800

    Add angular materials to the project and set up basic header of the website

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  //route to logincomponenet
+  { path: "login", component: LoginComponent },
+  { path: "feedback", component: FeedbackComponent },
+
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,64 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+//angular material
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from "@angular/material/card";
+import { MatOptionModule } from '@angular/material/core';
+import { MatDividerModule } from "@angular/material/divider";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from "@angular/material/list";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatTableModule } from "@angular/material/table";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCheckboxModule } from "@angular/material/checkbox"
+import { CdkTableModule } from '@angular/cdk/table';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    FeedbackComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+  
+    //angular material
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatListModule,
+        MatMenuModule,
+        MatTableModule,
+        MatDividerModule,
+        MatCardModule,
+        MatProgressSpinnerModule,
+        MatSlideToggleModule,
+        MatSelectModule,
+        MatOptionModule,
+        MatCheckboxModule,
+        CdkTableModule
+    
+    
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
